32d7811f33d70291d539c321b4c6dd803c192ad8
Venkatasubramanian <pvenkat.rv@gmail.com>
2020-08-09 12:15:24 +0530

    Update class-object.ts

--- samples/class-object.ts
+++ samples/class-object.ts
@@ -4,9 +4,22 @@ interface CarConfiguration {
     color: string;
     noOfDoor: number;
     brand: string;
+    engineCC: number;
 }
 
-class Car {
+class Engine {
+    engineNo: number;
+    piston: number;
+    boreDiameter: number;
+    engineCC: number;
+
+    constructor(cc: number) {
+        this.engineCC = cc;
+    }
+
+}
+
+class Car extends Engine {
 
     //properties
     color: string;
@@ -18,19 +31,16 @@ class Car {
     seatingType: string;
     navigation: string;
 
-    // constructor(carData: { carColor: string, door: number, brand: string; }) {
-    //     this.color = carData.carColor;
-    //     this.noOfDoor = carData.door;
-    //     this.brand = carData.brand;
-    // }
     constructor(carData: CarConfiguration) {
+        super(carData.engineCC);
+
         this.color = carData.color;
         this.noOfDoor = carData.noOfDoor;
         this.brand = carData.brand;
     }
 
-    getData(clr: string, door: number, brandType: string ='Benz', fuel?: string) {
-
+    getData(clr: string, door: number, brandType: string, fuel?: string) {
+        console.log(clr, door, brandType, fuel);
     }
 
 }
@@ -38,7 +48,8 @@ class Car {
 let car = new Car({
     "color": 'white',
     "noOfDoor": 2,
-    "brand": "BMW"
+    "brand": "BMW",
+    "engineCC": 1000
 });
 
 car.getData('red', 4, 'alto', 'gas');
